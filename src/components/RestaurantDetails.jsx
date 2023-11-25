b74dc8e7a718f16e03e92db052b96d3100788129
Suryansh Bhatnagar <sbh2068@gmail.com>
2023-11-25 23:06:27 +0530

    category data updated

--- src/components/RestaurantDetails.jsx
+++ src/components/RestaurantDetails.jsx
@@ -31,7 +31,7 @@ const RestaurantDetails = () => {
 
 
     return !restaurantInfo ? <RestaurantDetailsShimmer/> : <div className="flex flex-col mx-auto   px-5 lg:w-3/5 gap-5">
-        <section className="flex justify-between w-full bg-black rounded-b-md p-4">
+        {/* <section className="flex justify-between w-full bg-black rounded-b-md p-4">
             <div>
                 <h1 className="font-bold text-lg text-white mb-2">{restaurantInfo.name}</h1>
                 <h3 className="text-sm text-white">{truncateString(restaurantInfo.cuisines?.join(" ,"),30)}</h3>
@@ -45,12 +45,12 @@ const RestaurantDetails = () => {
              <div className="hidden md:block">
                 <img className="w-48 rounded-lg " src={IMAGE_CDN_URL + restaurantInfo.cloudinaryImageId} alt="restaurant"/>
             </div>
-        </section>
+        </section> */}
         <div className="border-b border-gray-300 w-full"></div>
         <div>
         <div>
             {
-                categories?.map((category,index)=><RestaurantCategory
+                category_data?.map((category,index)=><RestaurantCategory
                    key={index} 
                    data={category.card.card}
                    />)
